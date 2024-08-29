eef9537dd675d18edd557b4feba516254ca099d4
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-29 22:04:34 +0300

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """
+    
+    """
     for field in fields:
         pattern = r'{}=.*?{}'.format(re.escape(field), re.escape(separator))
         message = re.sub(pattern, f'{field}={redaction}{separator}', message)
