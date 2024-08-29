f8081928bc0a51e2e308b20b29d9a4d902c57a11
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-29 23:26:36 +0300

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,9 @@ from os import environ
 import mysql.connector
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
