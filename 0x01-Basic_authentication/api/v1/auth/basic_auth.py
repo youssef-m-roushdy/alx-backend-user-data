1bd41b32783f30aabbbad1e8c42b080c7d27221a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-02 23:30:11 +0300

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,24 @@ class BasicAuth(Auth):
     """
     A class for basic authentication that inherits from Auth.
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 encoded authorization token
+        from the Authorization header.
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+
+        # Split the authorization header into its components
+        data = authorization_header.split(" ")
+
+        # Check if the scheme is "Basic"
+        if data[0] != "Basic":
+            return None
+
+        # Return the Base64 encoded token
+        return data[1]
