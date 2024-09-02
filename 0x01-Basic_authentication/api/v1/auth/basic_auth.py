854f4dfed80d7e347bf381dcbc01036170465175
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-03 01:11:35 +0300

    .

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ and authentication in a Flask app.
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -65,3 +67,27 @@ class BasicAuth(Auth):
             return (None, None)
         return (decoded_base64_authorization_header.split(":")[0],
                 decoded_base64_authorization_header.split(":")[1])
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        Method returns the User instance based
+        on his email and password.
+        """
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+
+        # If no users are found with the given email, return None
+        if not users:
+            return None
+
+        user = users[0]
+
+        # Check if the provided password is valid for the found user
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
