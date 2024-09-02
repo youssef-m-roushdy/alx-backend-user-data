2a97819cf66be3f83a8e9ea60f3446ec58a055aa
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-02 21:37:03 +0300

    .

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 """
-Manage the API authentication
+Authentication module for handling user authorization
+and authentication in a Flask app.
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    """
+    A class to manage authentication in a web application.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Manage auth path
+        Determines if authentication is required for a given path.
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
-        Manage auth authorization header
+        Retrieves the authorization header from the request.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Manage current user auth
+        Retrieves the current user from the request.
         """
         return None
-    
