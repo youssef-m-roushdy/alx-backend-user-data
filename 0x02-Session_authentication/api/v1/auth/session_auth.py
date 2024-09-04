6ef6db3fd2f6b339c37fd74e35864343bb5de198
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-04 21:43:31 +0300

    .

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,20 @@ Authentication module for handling user authorization
 and authentication in a Flask app.
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session-based authentication class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Method that creates a Session ID for a user_id
+        """
+        if not user_id or type(user_id) != str:
+            return None
+
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
