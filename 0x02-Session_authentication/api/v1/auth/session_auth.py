cd840d51f85920fb3ccca975bb964fb567377b0b
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-04 21:34:13 +0300

    .

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Authentication module for handling user authorization
+and authentication in a Flask app.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    pass
