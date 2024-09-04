9aabdd8192f91e817972ac1400e7d55a3133195a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-04 21:49:58 +0300

    .

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,12 @@ class SessionAuth(Auth):
         session_id = uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Method that returns a User ID based on a Session ID
+        """
+        if not session_id or type(session_id) != str:
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
