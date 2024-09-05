8ec5698669e42bc7dc19114b7e4f13456bf24483
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-05 22:06:17 +0300

    .

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -27,6 +28,8 @@ elif auth_type == "session_auth":
     auth = SessionAuth()
 elif auth_type == "session_exp_auth":
     auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    auth = SessionDBAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" User module
+"""
+from models.base import Base
+
+class UserSession(Base):
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
