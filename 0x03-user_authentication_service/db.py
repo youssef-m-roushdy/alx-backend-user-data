a4d0fea60605a50636349bc1c31e13ba37f57f84
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-10 23:04:31 +0300

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,13 +31,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Creates new User instance and
-            saves them to the database.
-            Args:
-                - email
-                - hashed_password
-            Return:
-                - new User object
+        """
+        Adds a new user to the database.
+
+        Args:
+            email (str): The email of the user to be added.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
         """
         session = self._session
         try:
