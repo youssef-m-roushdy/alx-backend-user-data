322dd8f806d24e61fd8ab8c796124799d24fa3c3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-10 22:59:17 +0300

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine, tuple_
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
