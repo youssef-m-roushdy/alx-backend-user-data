7d71e3621599268dca94d2ee0389f07b4cb92829
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-10 22:49:01 +0300

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,12 @@ class DB:
         Returns:
             User: The newly created User object.
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        session = self._session
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            session.add(user)
+            session.commit()
+        except Exception:
+            session.rollback()
+            new_user = None
+        return new_user
