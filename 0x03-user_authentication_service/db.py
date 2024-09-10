23975803b0a5aa27023eed3ce3db60f344e19ec7
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-10 22:45:55 +0300

    .

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,47 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """
+        Adds a new user to the database.
+
+        Args:
+            email (str): The email of the user to be added.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
