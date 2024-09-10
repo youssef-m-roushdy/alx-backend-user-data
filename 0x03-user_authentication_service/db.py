40dcc5308df56c5456181b6afcf9b1820b3f9cd8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-10 23:13:08 +0300

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,8 +2,10 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+
 from user import Base, User
 
 
