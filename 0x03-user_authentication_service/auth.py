8cc98edf1eddab40111817d1a855cf0f25597eb3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-11 21:55:14 +0300

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,7 +72,7 @@ class Auth:
         
     def get_user_from_session_id(self, session_id: str) -> User | None:
         """
-        Methd return user by his session id
+        Retrieves a user based on their session ID.
         """
         db = self._db
         if not session_id or not db.find_user_by(session_id=session_id):
