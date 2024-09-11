725af519bdf6930a7f3ed29166194487103d4209
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-11 22:26:52 +0300

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,3 +83,14 @@ class Auth:
             return user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Method that destroy users session
+        """
+        db = self._db
+        try:
+            db.update_user(user_id, session_id=None)
+        except Exception:
+            pass
+        return None
