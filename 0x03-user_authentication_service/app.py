7d675ffccdeea892f0290b3fac595efc8a98a7b9
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-11 19:32:26 +0300

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,7 +28,7 @@ def create_user():
 
     try:
         AUTH.register_user(email, password)
-        return jsonify({"message": "User created", "email": email}), 201
+        return jsonify({"message": "User created", "email": email}), 200
     except Exception:
         return jsonify({"message": "Email already registered"}), 400
 
