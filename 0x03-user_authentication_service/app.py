310b0e0cf982de2a7e8da647a60b5614f5ef036d
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-12 00:40:08 +0300

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -117,7 +117,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/reset_password', methods=['POST'])
+@app.route('/reset_password', methods=['PUT'])
 def update_password():
     """
     Updates the user's password.
