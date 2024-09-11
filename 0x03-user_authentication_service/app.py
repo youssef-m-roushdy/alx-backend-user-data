df46ebd76b36b31c8139ac1dab9768b39de39ec7
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-12 00:11:45 +0300

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,5 +100,22 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+    Method to reset password_token
+
+    Returns:
+        - email and the reset_token
+        - Unauthorized
+    """
+    email = request.form.get('email')
+    try:
+        token_reset = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token_reset})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
