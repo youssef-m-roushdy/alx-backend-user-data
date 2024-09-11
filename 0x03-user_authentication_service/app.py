b610fc03815af9696984c60a7da586893cfa6465
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-12 00:36:15 +0300

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -117,5 +117,23 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def update_password():
+    """
+    Updates the user's password.
+    Returns:
+        - JSON response .
+        - 403 status code.
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        token_reset = AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
