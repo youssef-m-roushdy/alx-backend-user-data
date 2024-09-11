8a8831d66b9dd4b448f8d83289901a1907ff12fb
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-12 00:40:36 +0300

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -129,7 +129,7 @@ def update_password():
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token, new_password)  
+        AUTH.update_password(reset_token, new_password)
     except Exception:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"}), 200
